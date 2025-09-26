be658d83ac792308c9e058e912291a74611989a6
Patrick Rivers <Patrick.Rivers@baldwincountyal.gov>
2025-09-26 16:25:27 -0500

    Worked on Hero Section and responsive styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import HomePage from "./pages/home/page";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <HomePage />
   );
 }
