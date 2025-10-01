2e5b873e39733fe52b2098498aa581805f70008d
Patrick Rivers <Patrick.Rivers@baldwincountyal.gov>
2025-10-01 16:29:38 -0500

    Worked on Problem Section

--- src/app/pages/home/components/sections/problem_section.tsx
+++ src/app/pages/home/components/sections/problem_section.tsx
@@ -7,14 +7,14 @@ export default function ProblemSection() {
         <section className={styles.problem_section}>
             <div className={`${styles.problem_section_title_container}`}>
                 <h1>
-                    The 5 Worst Web Presence Mistakes Businesses Make
+                    The 5 Worst Web Presence Mistakes Small Businesses Make
                 </h1>
             </div>
 
             <div className={styles.problem_section_content}>
                 <ul>
                     <li>
-                        <h3>1. Generic:</h3><p></p>
+                        <h3 className="underline">1. Generic:</h3><p>The main problem I see with traditional websites, is they are not speaking to their customer. They list facts about the business, they list features, they talk about how big and great the business is, and they are loaded with distracting links that confuse customers. This is all well and good in certain contexts, but the customer has a problem they are looking for a solution to. A generic website, by nature, can not speak directly to the customer's specific needs. A traditional website is not there to present a specific offer to a specific customer in that customer's time of need. A traditional website relies on the customer to uncover the offer for themselves instead of the business being able to lay it out plainly and conveniently for the customer.</p>
                     </li>
                     <li>
                         <h3>2. Lack of Clear Call-to-Action (CTA):</h3><p>Visitors land on your site but have no clear, compelling instruction on what to do next. This leads to confusion and lost opportunities.</p>
